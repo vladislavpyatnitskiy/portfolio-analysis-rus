ffee0f11fcc1c08cd91c6540670b3124e8dae9ee
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-22 11:09:03 +0500

    Update Scatter Plot.R
    Optimised script:
    
    • tailored script for portfolio

--- Scripts for Plots/Scatter Plot.R
+++ Scripts for Plots/Scatter Plot.R
@@ -2,20 +2,29 @@ lapply(c("quantmod", "ggplot2", "ggrepel"), require, character.only = T) # Libs
 
 rus.scatter.plt <- function(x){ # Scatter plot of portfolio's securities
   
+  # Take column names containing tickers
+  c <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)][,-(ncol(x) / 3 + 1)])
+  
+  # Calculate Expected Returns
+  x <- x[,3*seq(ncol(x) %/% 3, from=1)] / x[,2+3*seq(ncol(x) %/% 3-1, from=0)]
+  
+  colnames(x) <- c # Put tickers to values
+  
+  x[is.na(x)] <- 0 # assign zeros to NaN values
+    
   d <- NULL # Empty variable to contain values
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
   
-    j <- diff(log(s))[-1] # Clean data to reduce NA and calculate return
-  
+    # Clean data to reduce NA and calculate return for ownership period  
+    j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
+    
     d <- rbind.data.frame(d, cbind(sd(j)*1000, (exp(sum(j))-1)*100)) } # Join
-  
-  # Plot
+    
   ggplot(d, mapping = aes(x = d[,1], y = d[,2])) + geom_point() +
     geom_text_repel(aes(label = colnames(x))) + theme_minimal() +
     theme(plot.title = element_text(hjust = .5)) +
-    scale_y_continuous(trans='log10') +
     labs(title = "Performance of Portfolio Securities",
-         x = "Risk (Standard Deviation)", y = "Return (%)")
+         x = "Risk (Standard Deviation)", y = "Return (%)") # Plot
 }
-rus.scatter.plt(rus.df1) # Test
+rus.scatter.plt(rus.portfolio.df) # Test
