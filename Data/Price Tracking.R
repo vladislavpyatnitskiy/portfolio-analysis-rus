adf353c5a26057283b4f7ed88646e2ba3b17b3df
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-06-27 18:31:03 +0500

    Update Price Tracking.R
    Updated and optimised script:
    
    • Reduced excessive lines
    • Combined functions
    • Added time of quotes

--- Data/Price Tracking.R
+++ Data/Price Tracking.R
@@ -2,13 +2,12 @@ library("rvest") # Library
 
 smartlab.portfolio <- function(x){ # Data Frame with current stocks info
   
-  l <- NULL # 
+  l <- NULL # Data Frame with Ticker and Number columns
   
-  for (n in 1:length(x)){ # Ticker & Number of stocks info
+  for (n in 1:length(x[[1]])){ # Ticker & Number of stocks info
+  
+    l<-rbind.data.frame(l,cbind(x[[1]][[n]],x[[4]][[n]][length(x[[4]][[n]])]))}
     
-    l <- rbind.data.frame(l, cbind(x[[n]][1],
-                                   x[[n]][4][[1]][length(x[[n]][4][[1]])])) }
-  
   colnames(l) <- c("Ticker", "Number") # Column names
   
   p <- read_html("https://smart-lab.ru/q/shares/") # Get info from website
@@ -17,11 +16,11 @@ smartlab.portfolio <- function(x){ # Data Frame with current stocks info
   
   f <- tab %>% html_nodes('tr') # Subtract nodes with tickers
   
-  L <- NULL # Reorganise data into data frame
+  L <- NULL # Reorganise data into data frame with ticker, price and return
   
   for (n in 2:length(f)){ j <- f[n] %>% html_nodes('td') %>% html_text()
   
-    P <- gsub('["\n"]', '', gsub('["\t"]', '', j[8]))
+    P <- gsub('["\n"]', '', gsub('["\t"]', '', j[8])) # Clean Data
     
     P <- as.character(read.fwf(textConnection(P), widths = c(nchar(P) - 1, 1),
                                colClasses = "character")[1])
@@ -30,16 +29,21 @@ smartlab.portfolio <- function(x){ # Data Frame with current stocks info
     
     L <- rbind.data.frame(L, cbind(j[3], j[7], P)) } # ticker, price & %
   
-  colnames(L) <- c("Ticker", "Price", "%") # Column names
+  H <- f[3] %>% html_nodes('td') %>% html_text() # Subtract row
   
-  S <- merge(L, l, by = "Ticker") # Join data
+  H <- H[10] # Subtract cell with time value
   
-  for (n in 2:4){ S[,n] <- as.numeric(S[,n]) } # Make data numeric
+  colnames(L) <- c("Ticker", "Price", "Return") # Column names
   
-  S$PnL <- as.numeric(format(as.numeric(S$Price) * (1 - 1 / (1 + S[,3] / 100)),
-                             scientific = F)) * as.numeric(S$Number)
+  S <- merge(L, l, by = "Ticker") # Join price, return & number data by ticker
   
-  S$Value <- as.numeric(S$Price) * S$Number # Total sum
+  for (n in 2:4){ S[,n] <- as.numeric(S[,n]) } # Change data format to numeric
+  
+  S$PnL <- as.numeric(format(S[,2] * (1-1/(1+S[,3]/100)),scientific=F)) * S[,4]
+  
+  S$Portion <- round((S$Price * S$Number / sum(S$Price * S$Number)) * 100, 2)
+  
+  S$Value <- S$Price * S$Number # Calculate P&L, Portions & Total Value
   
   b <- sum(S[seq(nrow(S)),6] / ((100 + S[seq(nrow(S)),3]) / 100)) # Last total
   
@@ -47,11 +51,12 @@ smartlab.portfolio <- function(x){ # Data Frame with current stocks info
   
   rownames(S) <- seq(nrow(S)) # Sort row names in an ascending way
   
-  d <- list(S, # Table of tickers, prices, %, Numbers & Value of Stocks
-            c(sprintf("Portfolio Value: %s Roubles", sum(S[,6])),
+  d <- list(sprintf("Time: %s", H), # Time
+            S, # Table of tickers, prices, %, Numbers & Value of Stocks
+            c(sprintf("Portfolio Value: %s Roubles", sum(S[,7])),
               sprintf("Total PnL: %s Roubles", round(sum(S[,5]), 2)),
               sprintf("Return: %s %%",round(((sum(S[,6]) - b) / b) * 100, 2)),
               sprintf("%s: %s %%", L[1,1], L[1,3]))) # Index Return
   d # Show
 }
-smartlab.portfolio(rus.nest.df) # Test
+smartlab.portfolio(pos.df.new) # Test
