b487867ebe46fe211dc661d809631e253098ca72
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-03-09 10:38:10 +0500

    Update Bar Plot of Returns.R
    Optimised and simplified script:
    
    • Reduced excessive lines
    • Rewrote part for grid
    • Rewrote part for colours of returns
    • Renamed variables

--- Scripts for Plots/Bar Plot of Returns.R
+++ Scripts for Plots/Bar Plot of Returns.R
@@ -1,55 +1,32 @@
 rus.bar.plt <- function(x){ # Bar Plot of Stocks Returns
   
-  # Take column names containing tickers
-  c <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)][,-(ncol(x) / 3 + 1)])
-  
   # Calculate Expected Returns
-  x <- x[,3*seq(ncol(x) %/% 3, from=1)] / x[,2+3*seq(ncol(x) %/% 3-1, from=0)]
+  v <- x[,3*seq(ncol(x) %/% 3, from=1)] / x[,2+3*seq(ncol(x) %/% 3-1, from=0)]
   
-  x[is.na(x)] <- 0 # assign zeros to NaN values
+  v[is.na(v)] <- 0 # assign zeros to NaN values
   
-  v <- NULL #
+  R <- NULL # Put Return values into this variables
   
-  for (n in 1:ncol(x)){ s <- x[,n] #
+  for (n in 1:ncol(v)){ s <- v[,n] # Calculate Return for each security
   
-  j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
-  
-  v <- c(v,  (exp(sum(j)) - 1) * 100) } # Join  
-  
-  names(v) <- c # Give column names
-  
-  v <- sort(v, decreasing = T) # Make data numeric and sort values
-  
-  mx <- ceiling(round(max(v)) / 10 ^ (nchar(round(max(v))) - 1)) *
-    10 ^ (nchar(round(max(v))) - 1) # Round maximum value up
-  
-  if (min(v) < 0){ # Round minimum value down
+    j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
-    if (any(seq(9) == as.numeric(strsplit(as.character(min(v)),"")[[1]][3]))){
-      
-      mn <- (as.numeric(strsplit(as.character(min(v)),"")[[1]][2]) + 1) * -10 }
-  }
+    R <- c(R,  (exp(sum(j)) - 1) * 100) } # Join values and assign tickers
+    
+  names(R) <- colnames(x[,1 + 3*seq(ncol(x) %/% 3, from=0)][,-(ncol(x)/3 + 1)])
+  
+  R <- sort(R, decreasing = T) # Make data numeric and sort values
+  
   # Bar Plot with securities returns
-  plt <- barplot(v, names.arg = names(v), horiz=T, xlab="Returns (%)", las=1,
-                 main = "Performance of Portfolio Securities", xlim=c(mn, mx),
-                 col=c(rep("green4",length(v)-sum(v<0)),rep("red3",sum(v<0)))) 
+  B <- barplot(R, names.arg = names(R), col = ifelse(R < 0, "red3", "green4"),
+               main = "Performance of Portfolio Securities", horiz = T,
+               xlim = c(min(R) - 1, max(R) + 1), xlab = "Returns (%)", las = 1) 
+
+  abline(h = B, col = "grey", lty = 3) # Horizontal lines
   
-  # Add vertical (returns) and horizontal (through bars) grey dotted lines
-  abline(h = plt, col = "grey", lty = 3) 
+  abline(v = 0) # Break Even Point
   
-  l <- as.numeric(v)[!is.na(as.numeric(v))] # Get values for intervals
-  
-  m <- round(min(l) * -1 + max(l),0)/10^(nchar(round(min(l) * -1+ max(l),0)))
-  
-  i <- c(0, 1, 2, 5) # Calculate intervals for lines and axes
-  
-  for (n in 1:length(i) - 1){ if (m > i[n] && m < i[n + 1]){
-    
-      mc <- i[n + 1] * 10 ^ (nchar(m) - 3) } else { next } }
-  
-  for (n in seq(from=-100,by=mc/5,to=10000)){ abline(v=n, col="grey", lty=3) } 
-  
-  axis(side=1, at = seq(from = -100, by = mc / 5, to = 10000), las=1) # X-axis
+  grid(nx = NULL, ny = 1, col = "grey", lwd = 1) # Vertical lines
   
   box() # Make borders for plot
 }
