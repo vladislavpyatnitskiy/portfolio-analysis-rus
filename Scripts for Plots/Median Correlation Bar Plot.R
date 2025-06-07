9736e483b5ff5e3e2e83c159c46f819d26942450
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-07 11:31:47 +0500

    Update Median Correlation Bar Plot.R
    Improved script readability

--- Scripts for Plots/Median Correlation Bar Plot.R
+++ Scripts for Plots/Median Correlation Bar Plot.R
@@ -26,46 +26,49 @@ rus.bar.plt.cor <- function(x){ # Bar Plot with Median Correlation Values
   
   for (n in 1:length(cor.names)){ k <- cor.names[n] # Select ticker
   
-  v <- f[f$`Security 1` == k | f$`Security 2` == k, ] # ticker's correlations
-  
-  l <- rbind.data.frame(l, cbind(median(v[,3]), mean(v[,3]))) } # Join
+    v <- f[f$`Security 1` == k | f$`Security 2` == k, ] # ticker's correlations
+    
+    l <- rbind.data.frame(l, cbind(median(v[,3]), mean(v[,3]))) } # Join
   
   rownames(l) <- cor.names
   colnames(l) <- c("Median", "Average") # Column names
   
   l <- l[order(l$Median), ] # Sort in an ascending way
   
-  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
-               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
-               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
-               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
-               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
-               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
-               "#bd5975") # Add colour range
+  C = c(
+    "#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+    "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+    "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+    "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+    "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+    "#895c8b","#bd5975"
+    ) # Add colour range
   
   # Create bar plot
-  bar.plt.script <- barplot(l[,1],
-                            names.arg = rownames(l),
-                            horiz = F,
-                            col = colors37,
-                            main = "Median Correlations of Portfolio Stocks",
-                            ylab = "Median Correlation Level",
-                            ylim = c(0, ceiling(max(l[,1]))),
-                            las = 2)
+  B <- barplot(
+    l[,1],
+    names.arg = rownames(l),
+    horiz = F,
+    col = C,
+    main = "Median Correlations of Portfolio Stocks",
+    ylab = "Median Correlation Level",
+    ylim = c(0, ceiling(max(l[,1]))),
+    las = 2
+    )
   # Y axis
   p.seq <- seq(0, .95, .05)
   axis(side = 2, at = p.seq, las = 1, labels = p.seq)
   axis(side = 4, at = seq(0, 1, .05), las = 1, labels = seq(0, 1, .05))
   
   for (n in p.seq){ abline(h = n, col ="grey", lty = 3) } # Horizontal lines
-  abline(v = bar.plt.script, col = "grey", lty = 3) # Vertical lines
+  abline(v = B, col = "grey", lty = 3) # Vertical lines
   
   c <- c("black", "red", "orange", "gold", "greenyellow", "green", "limegreen")
   v <- c(0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2)
   
   for (n in 1:length(v)){ abline(h = v[n], col = c[n], lwd = 2) } # Lines
   
-  par(mar = c(5, 5, 5, 5)) # Define borders of the plot
+  par(mar = rep(5, 4)) # Define borders of the plot
   
   box() # Add box
   
@@ -75,12 +78,16 @@ rus.bar.plt.cor <- function(x){ # Bar Plot with Median Correlation Values
   
   m <- NULL # Write advices about securities according to correlations
   
-  j <- list(list(.5, 1, "Sell these Assets:"),
-            list(.45, .5, "Sell one of these Assets:"), 
-            list(.4, .45, "Consider to sell one of these Assets:"),
-            list(.35, .4, "Check these Assets:"),
-            list(.3, .35, "OK to keep Assets:"), list(.25, .3, "Good Assets:"),
-            list(.2, .25, "Great Assets:"), list(-1, .2, "Best Assets:")) 
+  j <- list(
+    list(.5, 1, "Sell these Assets:"),
+    list(.45, .5, "Sell one of these Assets:"),
+    list(.4, .45, "Consider to sell one of these Assets:"),
+    list(.35, .4, "Check these Assets:"),
+    list(.3, .35, "OK to keep Assets:"),
+    list(.25, .3, "Good Assets:"),
+    list(.2, .25, "Great Assets:"),
+    list(-1, .2, "Best Assets:")
+    ) 
   
   for (n in 1:length(j)){ # Messages indicating correlation levels for stocks
     
