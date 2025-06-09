e68b9b7cf2d2a8fdb533b9dc47b255f9e00a3717
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-09 10:49:17 +0500

    Update Bar Plot of Returns.R
    Rewrote parts for grid and plot

--- Scripts for Plots/Bar Plot of Returns.R
+++ Scripts for Plots/Bar Plot of Returns.R
@@ -8,25 +8,31 @@ rus.bar.plt <- function(x){ # Bar Plot of Stocks Returns
   R <- NULL # Put Return values into this variables
   
   for (n in 1:ncol(v)){ s <- v[,n] # Calculate Return for each security
-  
+    
     j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
-    R <- c(R,  (exp(sum(j)) - 1) * 100) } # Join values and assign tickers
+    R <- c(R, (exp(sum(j)) - 1) * 100) } # Join values and assign tickers
     
   names(R) <- colnames(x[,1 + 3*seq(ncol(x) %/% 3, from=0)][,-(ncol(x)/3 + 1)])
   
   R <- sort(R, decreasing = T) # Make data numeric and sort values
   
   # Bar Plot with securities returns
-  B <- barplot(R, names.arg = names(R), col = ifelse(R < 0, "red3", "green4"),
-               main = "Performance of Portfolio Securities", horiz = T,
-               xlim = c(min(R) - 1, max(R) + 1), xlab = "Returns (%)", las = 1) 
-
+  B <- barplot(
+    R,
+    col = ifelse(R < 0, "red3", "green4"),
+    main = "Performance of Portfolio Securities",
+    horiz = T,
+    xlim = c(min(R) - 1, max(R) + 1),
+    xlab = "Returns (%)",
+    las = 1
+    ) 
+  
   abline(h = B, col = "grey", lty = 3) # Horizontal lines
   
-  abline(v = 0) # Break Even Point
-  
   grid(nx = NULL, ny = 1, col = "grey", lwd = 1) # Vertical lines
+  
+  abline(v = 0) # Break Even Point
   
   box() # Make borders for plot
 }
