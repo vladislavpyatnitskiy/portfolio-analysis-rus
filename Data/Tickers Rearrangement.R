8b5a820de7caf717811b667860f408968e2fb81c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-10 11:25:40 +0500

    Create Tickers Rearrangement.R
    Added script enabling to get a vector of tickers with ones you want to add and without ones you want to delete

---
+++ Data/Tickers Rearrangement.R
@@ -0,0 +1,37 @@
+rus.tickers <- function(x, del, add, info = F){ # Rearrange tickers
+  
+  # Extract tickers from portfolio data frame
+  
+  x = colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)][,-(ncol(x) %/% 3 + 1)]) 
+  
+  l <- NULL # Delete tickers you want to get rid of your portfolio
+  
+  for (n in 1:length(del)){ if (identical(grep(del[n], x), integer(0))){
+    
+      l <- c(l, del[n]) # Add excess tickers to a vector
+    
+      next } # Next round of the loop
+    
+    x <- x[-grep(del[n], x)] } # Delete tickers
+      
+  titles <- c("Tickers", "Number of tickers") # Number of tickers
+  
+  # Scenario when you want to know new and excessive tickers in the vector #
+  
+  if (info){ y <- list(c(x, add), length(c(x, add)), l, length(l))
+  
+    titles <- c(titles, "Excess tickers", "Number of Excess Tickers") }
+  
+  # Scenario when you just need to know tickers in new portfolio #
+    
+  else { y <- list(c(x, add), length(c(x, add))) } # tickers and their number
+  
+  names(y) <- titles # Names for lists
+  
+  y # Display
+}
+rus.tickers(rus.portfolio.df,
+            c("RASP", "QIWI", "MGNT", "ABRD", "PIKK", "NVTK", "UPRO",
+              "TTLK", "GCHE", "AKRN", "AQUA"),
+            c("BISVP", "RENI", "WTCMP", "MRKV"),
+            T)
