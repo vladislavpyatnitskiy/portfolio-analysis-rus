6a50cabb2034d69cc6a1a19e72d577f199cef8b0
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-08-05 11:03:47 +0500

    Update Histogram of Correlations.R
    Updated script:
    
    • Added new libraries such as moexer and xts
    • Added data extraction part
    • Rewrote part for plot, grid and normal distribution

--- Scripts for Plots/Histogram of Correlations.R
+++ Scripts for Plots/Histogram of Correlations.R
@@ -1,9 +1,26 @@
-library("timeSeries") # Library
+lapply(c("moexer", "timeSeries", "xts"), require, character.only = T) # Libs 
 
 rus.hist.plt.cor <- function(x){ # Histogram with Portfolio Correlation values
   
+  p <- NULL # Create an empty variable and get stock price data
+  
+  C <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)])[-(ncol(x) %/% 3 + 1)]
+  
+  for (a in C){ 
+    
+    D <- as.data.frame(get_candles(a, "2007-01-01", till = as.Date(Sys.Date()),
+                                   interval = 'daily')[,c(3,8)])
+    
+    D <- D[!duplicated(D),] # Remove duplicates
+    
+    p <- cbind(p, xts(D[, 1], order.by = as.Date(D[, 2]))) }
+  
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Eliminate NAs
+  
+  colnames(p) <- C # Column names
+  
   # Calculate correlation matrix
-  cor_matrix <- cor(as.matrix(diff(log(as.timeSeries(x)))[-1,]))
+  cor_matrix <- cor(as.matrix(diff(log(as.timeSeries(p)))[-1,]))
   
   # Extract unique pairs and their correlations
   cor_pairs <- which(upper.tri(cor_matrix, diag = TRUE), arr.ind = TRUE)
@@ -26,24 +43,30 @@ rus.hist.plt.cor <- function(x){ # Histogram with Portfolio Correlation values
   s.max <- max(s) # Maximum value
   
   # Parameters
-  hist(s, main = "Portfolio Correlations Histogram", freq = F, breaks=100,
-       ylab = "Frequency", xlab = " Unique Correlation Values", las = 1,
-       xlim = c(s.min, s.max), col = "navy", border = "white")
+  hist(
+    s,
+    main = "Portfolio Correlations Histogram",
+    freq = F,
+    breaks=100,
+    ylab = "Frequency",
+    xlab = " Unique Correlation Values",
+    las = 1,
+    xlim = c(s.min, s.max),
+    col = "navy",
+    border = "white"
+    )
   
-  for (n in seq(round(s.min, 1), round(s.max, 1), by=.05)){ # Add grey lines
-    
-    abline(v = n, col = "grey", lty = 3) } # Add Vertical lines
+  R <- seq(round(s.min, 2), round(s.max, 2), by = .0001)
   
-  abline(v = 0, col = "black", lwd = 2) # Add vertical line at x = 0
+  lines(
+    R, 
+    dnorm(R, mean(s), sd(s)), col = "red", lwd = 2
+    )
   
-  lines(seq(round(s.min, 2), round(s.max, 2), by=.0001), # Normal Distribution
-        dnorm(seq(round(s.min, 2), round(s.max, 2), by = .0001),
-              mean(s), sd(s)), col = "red", lwd = 2)
+  grid(nx = NULL, ny = NULL, lty = 3, col = "grey") # Horizontal lines
   
-  for (n in seq(0,100,1)){ abline(h=n,col="grey",lty=3) } # Horizontal lines
-  
-  axis(side = 1, at = seq(-1, 1, 0.1)) # Horizontal axis values
+  abline(h = 0) # Add vertical line at x = 0
   
   box() # Define plot borders
 }
-rus.hist.plt.cor(rus.df1) # Test
+rus.hist.plt.cor(rus.portfolio.df) # Test
