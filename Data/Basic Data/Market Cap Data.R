1f08392822368e4254c82b2899475838bf9247b9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-06 15:44:54 +0500

    Update Market Cap Data.R
    Revived script

--- Data/Basic Data/Market Cap Data.R
+++ Data/Basic Data/Market Cap Data.R
@@ -4,26 +4,29 @@ rus.marketcap <- function(x, y){ # Portfolio Securities by Market Cap
   
   l <- NULL # Store data here
   
-  for (m in 1:length(y)){ v <- y[m] # For each ratio get Smart-lab.ru HTML
+  for (m in 1:length(y)){ v <- y[m] # For each ratio get Smartlab HTML
   
-    d<-read_html(sprintf("https://smart-lab.ru/q/shares_fundamental/?field=%s",
-                         v)) %>% html_nodes('table') %>% .[[1]] %>%
-      html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    d <- read_html(sprintf("https://smart-lab.ru/q/%s/?field=%s",
+                           "shares_fundamental",v)) %>% html_nodes('table') %>%
+      .[[1]] %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    
+    message(sprintf("%s is downloaded", gsub("_", "/", toupper(v))))
     
     D <- NULL # Variable for Table with Name, Ticker and values
     
-    for (n in 0:(length(d)/6)){ D <- rbind(D, cbind(d[(3 + n * 6)],
-                                                    d[(6 + n * 6)])) }
+    for (n in 0:(length(d)/6)){ D <- rbind(D, cbind(d[(3+n*7)],d[(6+n*7)])) }
+    
     D <- D[-nrow(D),] # Reduce last row
-    D[,2] <- gsub('["\n"]', '', gsub('["\t"]', '', D[,2])) # Clean messy data
     
-    for (n in 1:length(D)){ if (isTRUE(grepl(" ", D[n]))){ # Where gaps exist
-      
-        D[n] <- gsub(" ", "", D[n]) } } # Reduce gaps in market cap values
+    D[,2] <- gsub('["\n"]', '', gsub('["\t"]', '', D[,2]))
     
-    colnames(D) <- c("Ticker", gsub("_", "/", toupper(y[m]))) # Column names
+    D <- gsub(" ", "", D) # Reduce gap in market cap
     
-    if (is.null(l)){ l<-D } else { l<-merge(x=l,y=D,by="Ticker",all=T)} }# Join
+    colnames(D) <- c("Ticker", gsub("_", "/", toupper(v))) # Column names
+    
+    D <- subset(D, !apply(D == "", 1, any)) # Reduce empty row
+    
+    if (is.null(l)){ l <- D } else { l <- merge(x=l,y=D,by="Ticker",all=T) } } 
     
   if (isTRUE(l[1,1] == "")){ l <- l[-1,] } # Reduce empty row
   
@@ -36,7 +39,7 @@ rus.marketcap <- function(x, y){ # Portfolio Securities by Market Cap
   x <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)])[-(ncol(x) %/% 3 + 1)]
   
   if (isTRUE(any(x == "QIWI"))){ x <- x[x != "QIWI"] # If there is QIWI ticker
-  
+      
     Q <- read_html("https://www.kommersant.ru/quotes/us74735m1080") %>%
       html_nodes('article') %>% html_nodes('section') %>% html_nodes("div") %>%
       html_nodes("p") %>% html_text() %>% .[8] # Market Cap Value for QIWI
@@ -47,7 +50,7 @@ rus.marketcap <- function(x, y){ # Portfolio Securities by Market Cap
     
     colnames(Q) <- "Market Cap" # Column Name
     rownames(Q) <- "QIWI" } # ticker
-  
+    
   L <- NULL # Collect fundamental data for portfolio securities
   
   for (n in 1:length(x)){ L <- rbind.data.frame(L, l[x[n],]) } # Join data
