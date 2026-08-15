b4294a0041904313a2fb8df5e96832974c65e0f6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-08-15 11:32:42 +0500

    Enhance rus.heatmap.plt with method parameter
    Added method parameter to rus.heatmap.plt function for correlation calculation. Improved heatmap display and title handling.

--- Scripts for Plots/Heatmap.R
+++ Scripts for Plots/Heatmap.R
@@ -1,15 +1,21 @@
 lapply(c("moexer", "timeSeries", "xts"), require, character.only = T) # Libs 
 
-rus.heatmap.plt <- function(x, size = .9, main = NULL){
+rus.heatmap.plt <- function(x, size = .9, method = "spearman", main=NULL){
   
   p <- NULL # Create an empty variable and get stock price data
   
-  C = colnames(x[,1+3*seq(ncol(x) %/% 3,from=0)])[-(ncol(x)%/%3+1)]
+  C = colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from=0)])[-(ncol(x) %/% 3 + 1)]
   
   for (a in C){ 
     
-    D <- as.data.frame(get_candles(a, "2007-01-01", till = as.Date(Sys.Date()),
-                                   interval = 'daily')[,c(3,8)])
+    D <- as.data.frame(
+      get_candles(
+        a, 
+        "2007-01-01", 
+        till = as.Date(Sys.Date()), 
+        interval = 'daily'
+        )[,c(3,8)]
+      )
     
     message(
       sprintf(
@@ -30,34 +36,60 @@ rus.heatmap.plt <- function(x, size = .9, main = NULL){
   
   c.correlation = ncol(m.correlation) # Get number of columns
   
-  new_cor <- cor(m.correlation) # Calculate correlation coefficients
+  new_cor <- cor(m.correlation, method=method) # Calculate correlation
   
-  # Create appropriate colour for each pair of correlation for heatmap
+  # Create appropriate colour for each pair of correlation for heat map
   k.c <- round((10 * length(unique(as.vector(new_cor))))/2)
-  corrColorMatrix <- rgb(c(rep(0, k.c), seq(0, 1, length = k.c)),
-                         c(rev(seq(0,1,length=k.c)), rep(0,k.c)), rep(0,2*k.c))
-  # Display heatmap
-  image(x = 1:c.correlation,y = 1:c.correlation,z = new_cor[, c.correlation:1],
-        col = corrColorMatrix, axes = FALSE, main = "", xlab = "", ylab = "")
+  
+  corrColorMatrix <- rgb(
+    c(rep(0, k.c), seq(0, 1, length = k.c)),
+    c(rev(seq(0, 1, length = k.c)), rep(0, k.c)), 
+    rep(0, 2 * k.c)
+    )
+  
+  # Display heat map
+  image(
+    x = 1:c.correlation,
+    y = 1:c.correlation,
+    z = new_cor[, c.correlation:1],
+    col = corrColorMatrix, 
+    axes = FALSE, 
+    main = "", 
+    xlab = "", 
+    ylab = ""
+    )
   
   # Add labels for both axis
   axis(2, at = c.correlation:1, labels = colnames(m.correlation), las = 2)
   axis(1, at = 1:c.correlation, labels = colnames(m.correlation), las = 2)
   
-  title(main = main) # Add title for heatmap
+  title = ifelse(
+    !is.null(main), 
+    main, 
+    sprintf(
+      "Portfolio Correlations Heatmap by %s method", 
+      paste0(toupper(substr(method, 1, 1)), substr(method, 2, nchar(method)))
+    )
+  )
   
-  box() # Box heatmap
+  title(main = title) # Add title for heat map
   
-  # Add correlation values as text strings to each heatmap cell
+  box() # Box heat map
+  
+  # Add correlation values as text strings to each heat map cell
   x = y = 1:c.correlation
   n_x = n_y = length(y)
   xoy = cbind(rep(x, n_y), as.vector(matrix(y, n_x, n_y, byrow = TRUE)))
   coord_for_corr = matrix(xoy, length(y) ^ 2, 2, byrow = FALSE)
   X_for_corr = t(new_cor)
+  
   for (i in 1:c.correlation ^ 2) {
-    text(coord_for_corr[i, 1], coord_for_corr[c.correlation ^ 2 + 1 - i, 2],
-         round(X_for_corr[coord_for_corr[i,1],coord_for_corr[i,2]],digits = 2),
-         col = "white", cex = size) }
+    text(
+      coord_for_corr[i, 1], 
+      coord_for_corr[c.correlation ^ 2 + 1 - i, 2],
+      round(X_for_corr[coord_for_corr[i,1], coord_for_corr[i,2]],digits = 2),
+      col = "white", cex = size
+    ) 
+  }
 }
-rus.heatmap.plt(rus.portfolio.df, size =.9,
-                main = "Portfolio Correlations Heatmap") # Test
+rus.heatmap.plt(rus.portfolio.df, size =.9) # Test
